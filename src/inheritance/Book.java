6aa28b62d5b20da75d9678c285a6956a9e0fb80a
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-10 18:31:31 +0100

    my codes

--- src/inheritance/Book.java
+++ src/inheritance/Book.java
@@ -1,18 +1,18 @@
 package inheritance;
 
 public class Book<T>{
-    private String name;
-    private String yearOfPublication;
-    private String authour;
-    public Book(String name, String yearOfPublication, String authour){
+    private final String name;
+    private final String yearOfPublication;
+    private final String author;
+    public Book(String name, String yearOfPublication, String author){
         this.name = name;
-        this.authour= authour;
+        this.author= author;
         this.yearOfPublication = yearOfPublication;
     }
     public String toString(){
         return String.format("""
 %s : %s
 %s : %s
-%s : %s""", "Book Name", this.name,"Year of Publication", this.yearOfPublication,"Authour", this.authour);
+%s : %s""", "Book Name", this.name,"Year of Publication", this.yearOfPublication,"Authour", this.author);
     }
 }
