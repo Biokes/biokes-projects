cbee99e5160d1f31c31c7daf90fd0dac0ac58ea5
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-10 23:05:21 +0100

    my codes

--- src/inheritance/BookNerd.java
+++ src/inheritance/BookNerd.java
@@ -4,29 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookNerd{
-    private final Book<String>[] book;
+
     private List<Book> booksRead = new ArrayList<>();
 
-    public BookNerd(Book<String>... givenBook){
-        this.book = givenBook;
+
+    public void readBook(Book<String> givenBook){
+        this.booksRead.add(givenBook);
     }
-    public void readBook(Book<Book> book){
-        this.booksRead.add(book);
-    }
-    public StringBuilder getBooksRead(){
+    public String getBooksRead(){
         StringBuilder output = new StringBuilder();
         for( Book value : booksRead ){
             output.append(value.toString( ));
             output.append("\n");
         }
-        return output;
+        return output.toString();
     }
     public static void main(String[] args){
         Book<String> book= new AudioBook("name","2001","me",12.6,
                 12.56, "artist");
         Book<String> book2 = new PrintBook("name1", "2000", "me1", "antman", "9013-12");
         Book<String> book3 = new Book<>("nanme", "9002", "no one");
-        BookNerd user = new BookNerd(book, book3, book2);
+        BookNerd user = new BookNerd();
+        user.readBook(book);
+        user.readBook(book3);
+        user.readBook(book2);
         System.out.println(user.getBooksRead( ));
     }
 }
