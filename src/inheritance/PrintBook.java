db8b5e9656cc3b29b37fb20433ad96fdf0267a69
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-10 18:37:51 +0100

    my codes

--- src/inheritance/PrintBook.java
+++ src/inheritance/PrintBook.java
@@ -1,7 +1,7 @@
 package inheritance;
 
 public class PrintBook extends Book<String>{
-    private String publisher;
+    private final String publisher;
     public PrintBook(String name, String yearOfPublication, String author, String publisher){
         super(name, yearOfPublication, author);
         this.publisher = publisher;
