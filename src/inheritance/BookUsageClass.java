03522c5acf5d3e296b0d8618cf4d03640af1b6c2
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-10 19:03:28 +0100

    my codes

--- src/inheritance/BookUsageClass.java
+++ src/inheritance/BookUsageClass.java
@@ -2,10 +2,10 @@ package inheritance;
 
 public class BookUsageClass{
     public static void main(String[] args){
-        Book book = new AudioBook("name","2001","me",12.6,
+        Book<String> book= new AudioBook("name","2001","me",12.6,
                 12.56, "artist");
-        Book book2 = new PrintBook("name1", "2000", "me1", "antman", "9013-12");
-        Book book3 = new Book("nanme", "9002", "no one");
+        Book<String> book2 = new PrintBook("name1", "2000", "me1", "antman", "9013-12");
+        Book<String> book3 = new Book<>("nanme", "9002", "no one");
         System.out.println(book2);
         System.out.println(book);
         System.out.println(book3);
