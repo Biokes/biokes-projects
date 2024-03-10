3beef2d7d1692b77db916f6f0b3a6c646992e4c0
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-10 19:14:40 +0100

    my codes

--- src/inheritance/BookUsageClass.java
+++ src/inheritance/BookUsageClass.java
@@ -1,6 +1,29 @@
 package inheritance;
 
+import assignmentsAndTasks.MySet;
+
+import java.sql.Struct;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookUsageClass{
+    private final Book<Book> book;
+    private List<Book> booksRead = new ArrayList<>();
+
+    public BookUsageClass(Book<Book> givenBook){
+        this.book = givenBook;
+    }
+    public void readBook(Book<Book> book){
+        this.booksRead.add(book);
+    }
+    public StringBuilder getBooksRead(){
+        StringBuilder output = new StringBuilder();
+        for( Book value : booksRead ){
+            output.append(value.toString( ));
+            output.append("\n");
+        }
+        return output;
+    }
     public static void main(String[] args){
         Book<String> book= new AudioBook("name","2001","me",12.6,
                 12.56, "artist");
