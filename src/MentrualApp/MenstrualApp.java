71fa3fa9b1419846573355a6dd3c6f5544fcb6fe
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-13 07:57:35 +0100

    code

--- src/MentrualApp/MenstrualApp.java
+++ src/MentrualApp/MenstrualApp.java
@@ -18,12 +18,12 @@ public class MenstrualApp {
     public String getFertileDayEnd(){
         return fertileDayEnd.toString( ).replaceAll("-","/");
     }
-public void setCycleLength(int days){
+    public void setCycleLength(int days){
         if(days < 1)
             throw new IllegalArgumentException("Invalid cycle length");
     this.cycleLength = days;
 }
-private int getCycleLength(){
+    private int getCycleLength(){
         return cycleLength;
 }
     public String getNextStart(String lastFlowEndDate){
@@ -65,6 +65,9 @@ private int getCycleLength(){
             System.out.println("Enter your Last Menstruation End Date(dd/mm/yyyy): ");
             String date=scanner.next( );
             setLastFlowStop(date);
+            getNextStart(date);
+            length();
+            System.out.println(this);
         }catch(InvalidDateException exception){
             System.out.println();
             date();
@@ -74,15 +77,24 @@ private int getCycleLength(){
      return String.format("""
              According to the details provided::
              Next Flow Starts: %s
-             Next Flow Ends : %s
              Ovulation Date : %s
-             
-             
-             """);
+             Fetile Days is Expected to start on : %s.
+             And End on : %s
+             Disclaimer:: This is just an Assumption on the provided Details.
+             """, nextFlowStart,getNextOvulationDate(),fertileDayStart,fertileDayEnd);
     }
     public void appMain(){
         date();
     }
-
+    public void length(){
+        try{
+            System.out.println("Enter your cycle length: ");
+            int length=scanner.nextInt( );
+            this.cycleLength = length;
+        }catch(Exception exception){
+            System.out.println(exception.getMessage( ));
+            length();
+        }
+    }
 
 }
