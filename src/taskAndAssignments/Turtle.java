88c295104c1abbd9f555a47c85326697401a5116
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-13 20:08:48 +0100

    my wowkspace
    Signed-off-by: Biokes <106444765+Biokes@users.noreply.github.com>

--- src/taskAndAssignments/Turtle.java
+++ src/taskAndAssignments/Turtle.java
@@ -2,6 +2,13 @@ package taskAndAssignments;
 
 public class Turtle{
     public boolean isPenUp(){
+        return true;
+    }
+
+    public void penDown(){
+    }
+
+    public boolean penIsDown(){
         return false;
     }
 }
--- test/assignmentsAndTasks/TurtleTest.java
+++ test/assignmentsAndTasks/TurtleTest.java
@@ -1,14 +1,32 @@
 package assignmentsAndTasks;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import taskAndAssignments.Turtle;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TurtleTest{
+
+    private Turtle pet;
+
+    @BeforeEach
+    private void initializer(){
+        pet=new Turtle( );
+
+
+    }
     @Test
-    void testPenIsUP(){
-        Turtle pet=new Turtle( );
+    public void testPenIsUP(){
         assertTrue(pet.isPenUp( ));
+
+    }
+
+    @Test
+    public void testPenIsUp_takePenDown(){
+        assertTrue(pet.isPenUp( ));
+        pet.penDown( );
+        assertFalse(pet.penIsDown( ));
     }
 }
