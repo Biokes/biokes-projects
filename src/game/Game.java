0a0082c684a6e4b5f63855e048d690aa29857c39
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-14 19:17:49 +0100

    my workspace
    Signed-off-by: Biokes <106444765+Biokes@users.noreply.github.com>

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,7 @@
 package game;
 
+import static TicTacToe.CellDisplays.O;
+import static TicTacToe.CellDisplays.X;
 import static game.Cell.*;
 
 public class Game{
@@ -25,13 +27,43 @@ public class Game{
             return X;
         return O;
     }
-
     public void play(int[] position){
+        if( checkWinner( )!=null||boardIsFull( ) )
+            throw new WinnerExistException("There is a winner already");
         board[position[0]][position[1]]=button( );
         count++;
     }
 
-    private void checkWinner(){
+    private String checkWinner(){
+        if( (board[0][0]==X&&board[0][1]==X&&board[0][2]==X)||
+                    (board[1][0]==X&&board[1][1]==X&&board[1][2]==X)||
+                    (board[2][0]==X&&board[2][1]==X&&board[2][2]==X)||
+                    (board[0][0]==X&&board[1][0]==X&&board[2][0]==X)||
+                    (board[0][1]==X&&board[1][1]==X&&board[2][1]==X)||
+                    (board[0][2]==X&&board[1][2]==X&&board[2][2]==X)||
+                    (board[0][0]==X&&board[1][1]==X&&board[2][2]==X)||
+                    (board[2][0]==X&&board[1][1]==X&&board[0][2]==X) )
+            return "Player 1 wins";
+        else if( (board[0][0]==O&&board[0][1]==O&&board[0][2]==O)||
+                         (board[0][1]==O&&board[1][1]==O&&board[2][1]==O)||
+                         (board[0][2]==O&&board[1][2]==O&&board[2][2]==O)||
+                         (board[0][0]==O&&board[1][0]==O&&board[2][0]==O)||
+                         (board[1][0]==O&&board[1][1]==O&&board[1][2]==O)||
+                         (board[2][0]==O&&board[2][1]==O&&board[2][2]==O)||
+                         (board[0][0]==O&&board[1][1]==O&&board[2][2]==O)||
+                         (board[2][0]==O&&board[1][1]==O&&board[0][2]==O) )
+            return "Player 2 wins.";
+        else if( boardIsFull( ) )
+            return "Draw";
+        return null;
+    }
 
+    private boolean boardIsFull(){
+        int check=0;
+        for( int counter=0; counter<3; counter++ )
+            for( int count=0; count<3; count++ )
+                if( board[counter][count]==EMPTY )
+                    check++;
+        return check==0;
     }
 }
