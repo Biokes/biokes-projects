6ca8c4b982d97478f345276e3e333c351d4035fe
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-17 03:58:18 +0100

    my workspace
    Signed-off-by: Biokes <106444765+Biokes@users.noreply.github.com>

--- src/eStore/Cart.java
+++ src/eStore/Cart.java
@@ -5,19 +5,15 @@ import java.util.List;
 
 public class Cart{
     private final List<StoreItem> cart=new ArrayList<>( );
-
     public void addItem(StoreItem item){
         cart.add(item);
     }
-
     public int numberOfItems(){
-
         return cart.size( );
     }
     public StoreItem getItem(int index){
         return cart.get(index);
     }
-
     public void remove(String itemName){
         for( int count=0; count<numberOfItems( ); count++ ){
             if( getItem(count).getProductName( ).equalsIgnoreCase(itemName) ){
