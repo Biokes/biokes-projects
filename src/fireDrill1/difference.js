e64f85f9d9e032fad6e6919ef25d346bfb753e93
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-28 12:23:18 +0100

    refactoring a java Script drill
    Signed-off-by: Biokes <106444765+Biokes@users.noreply.github.com>

--- src/fireDrill1/difference.js
+++ src/fireDrill1/difference.js
@@ -1,22 +1,47 @@
-class Person {
-    minMAx(numbers) {
-        let min = numbers[0]
-        let max = numbers[0]
-        for (let count = 0; count < numbers.length; count++) {
-            if (count < min) min = count
-            if (count > max) max = count
-        }
-        return [max - min]
-    }
-}
-
-person = new Person()
-let value = [2, 4, 5, 7, 8];
-console.log(person.minMAx(value))
-var num = 5
-console.log(num)
-var num = 7
-console.log(num)
-let num = 5
-console.log(num)
-const nam = 7
+// const prompt = require('prompt-sync')();
+//
+// class Person {
+//     minMAx(numbers) {
+//         let min = numbers[0]
+//         let max = numbers[0]
+//         for (let count = 0; count < numbers.length; count++) {
+//             if (count < min) min = count
+//             if (count > max) max = count
+//         }
+//         return [max - min]
+//     }
+// }
+//
+// person = new Person()
+// let value = [2, 4, 5, 7, 8];
+// console.log(person.minMAx(value))
+// var x  = prompt("Entr a value for number1: ")
+// var y = prompt("Enter a number: ")
+// console.log(parseInt(2)* (parseInt(x)+parseInt(y)))
+// var x = prompt("Enter a value for number1: ")
+// console.log(parseInt(x)+ parseInt(x)%10)
+// var x = parseInt(prompt("Enter a value for number1: "))
+// let n = parseInt(prompt("Enter a value for number1: "))
+// console.log(x**n)
+// let a  = parseInt(prompt("Enter a number: "))
+// var x  = parseInt(prompt("Enter a number: "))
+// var y  = parseInt(prompt("Enter a number: "))
+// var z  = parseInt(prompt("Enter a number: "))
+// console.log("a = ", a/ (x*y*z))
+// console.log("x = ", a/ (x*y*z) + 10)
+// console.log("y = ", a/ (x*y*z) - 10)
+// console.log(typeof null)
+// ;let num = Array(5);
+var number = Number();
+var available = Boolean()
+var count = Number()
+var name = String()
+var author = String()
+var value = [number, available, count, name, author]
+console.log(value)
+number = 5
+available = false
+count = 56
+name = `nam`
+author = "name given"
+console.log(value)
