f4bddf24a39e6a880ffd18ca6cf8ffebcc0067d7
Biokes <106444765+Biokes@users.noreply.github.com>
2024-05-06 11:53:31 +0100

    serualizing java objects into json objects

--- src/assignmentsAndTasks/test/JsonSerializerTest.java
+++ src/assignmentsAndTasks/test/JsonSerializerTest.java
@@ -1,4 +1,10 @@
 package assignmentsAndTasks.test;
 
+import org.junit.jupiter.api.Test;
+
 public class JsonSerializerTest {
+    @Test
+    public void testSeralization(){
+        System.out.println( );
+    }
 }
